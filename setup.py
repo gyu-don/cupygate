1afdcda302a8027498afc50636d773b344dd26f1
gyu-don <takumi.kt+git@gmail.com>
2018-09-12 17:04:16 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name = "cupygate",
+    version="0.0.1alpha",
+    author="MDR Inc.",
+    author_email="kato@mdrft.com",
+    description="Quantum gate simulator",
+    url="https://github.com/gyu-don/cupygate",
+    license="Apache 2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+    ]
+)
